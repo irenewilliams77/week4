90dd5d670ca7f46d25999522ef00728d55208e4e
Irene Williams <europeandistribution@ymail.com>
2020-07-16 12:44:35 -0400

    created fun.js

---
+++ fun.js
@@ -0,0 +1,52 @@
+
+
+function greet(name){
+    console.log('Hello'+" " + name);
+}
+greet("Irene");
+
+function adding(sum){
+    return number + number1
+}
+var number = 5; 
+var number1 = 3;
+console.log(number +number1);
+
+function multiply(number2){
+    return number * number1;
+}
+var number2 = number* number1;
+console.log(number2);
+console.log(number*number1);
+
+function counter(beginning, end) {
+for (beginning; beginning <=end; beginning++) {
+    console.log(beginning); 
+    }
+}
+
+counter(5,9)
+var noi = "Hello"
+
+function noi(){
+    return noi;
+}
+
+console.log(noi);
+
+var anumber = 10;
+myFunction();
+function myFunction(){
+    anumber = 20;
+}
+//alert("Value of 'anumber' outside the fuction" + anumber);
+
+iifefunction();
+function iifefunction(){
+    alert("This is my successfully created iife")
+}
+
+const noise = function (){
+console.log("Beep");
+}
+noise();
